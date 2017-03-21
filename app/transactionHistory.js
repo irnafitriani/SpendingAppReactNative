4afb67b3aa770d8cb00127746c218945c3356764
Mohammad_T <mohammad.trinoferianto@mitrais.com>
2017-03-21 12:38:48 +0700

    add sorting function

--- app/transactionHistory.js
+++ app/transactionHistory.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+    Image,
     ListView,
     Picker,
     StyleSheet,
@@ -11,12 +12,16 @@ import ActionButton from 'react-native-action-button'
 import Firebase from 'firebase'
 import TransactionRow from './transactionRow'
 
+const sortIcon = require("../images/sort.png");
+
 export default class TransactionHistory extends React.Component {
     constructor(props) {
         super(props)
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
         this.state = {
             dataSource: ds,
+            sortCategory: '',
+            tempTrans: []
         }
         this.transRef = Firebase.database().ref();
     }
@@ -32,18 +37,44 @@ export default class TransactionHistory extends React.Component {
         this.listenForTaskRef(this.transRef.orderByChild('amount'))
     }
 
+    setTransList() {
+        console.log('set transaction list')
+    }
+
     listenForTaskRef(transRef) {
         transRef.on('value', (transactions) => {
             var newTransactions = [];
             transactions.forEach((transaction) => {
+                console.log('key : ' + transaction.key)
                 newTransactions.push({
                     key: transaction.key, name: transaction.val().name, amount: transaction.val().amount, date: transaction.val().date
                 })
             })
 
             this.setState({
+                tempTrans: newTransactions,
                 dataSource: this.state.dataSource.cloneWithRows(newTransactions)
             })
+        })
+    }
+
+    onPickerChange(picker) {
+        this.setState({sortCategory: picker})
+        this.listenForTaskRef(this.transRef.orderByChild(picker))
+    }
+
+    onSortPressed() {
+        console.log('sorting is pressed')
+        var temp = []
+        var i = this.state.tempTrans.length - 1
+        this.state.tempTrans.forEach((trans) => {
+            temp[i] = trans
+            i--
+        })
+
+        this.setState({
+            tempTrans: temp,
+            dataSource: this.state.dataSource.cloneWithRows(temp)
         })
     }
 
@@ -58,11 +89,23 @@ export default class TransactionHistory extends React.Component {
     render() {
         return(
             <View style={styles.container}>
-                <View style={styles.wrapPicker}>
-                    <Picker mode='dropdown' style={styles.picker}>
+                <View style={styles.toolbar}>
+                    <Text style={styles.label}>Sort by: </Text>
+                    <Picker 
+                        mode='dropdown' 
+                        onValueChange={(picker) => this.onPickerChange(picker)}
+                        selectedValue={this.state.sortCategory}
+                        style={styles.picker}>
                         <Picker.Item label='amount' value='amount' />
                         <Picker.Item label='date' value='date' />
                     </Picker>
+                    <TouchableHighlight
+                        onPress={this.onSortPressed.bind(this)}>
+                        <Image 
+                            source={sortIcon}
+                            style = {styles.icon}
+                            resizeMode="contain"/>
+                    </TouchableHighlight>
                 </View>
                 <ListView
                     dataSource={this.state.dataSource}
@@ -84,16 +127,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    icon:{
+        width: 20,
+        height: 20
+    },
+    label:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     list: {
         flex: 1,        
     },
     picker: {
-        flexDirection: 'column-reverse',
+        flexDirection: 'row-reverse',
         width: 100,
 
     },
-    wrapPicker: {
-        flex: 'row',
-        flexDirection: 'flex-end',
+    toolbar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        borderColor: '#adadad',
+        borderBottomWidth: 1,
     }
 });
