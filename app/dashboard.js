eb7aa1fa3112954437534ebac3c739cb76d5e8e2
Mohammad_T <mohammad.trinoferianto@mitrais.com>
2017-03-27 12:37:56 +0700

    add image for prev and next icon, add prev and next UI

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import {
+    Image,
     View,
     Text,
+    Platform,
     StyleSheet,
+    TouchableHighlight,
 } from 'react-native'
 import Firebase from 'firebase'
 import { Bar, StockLine, SmoothLine } from 'react-native-pathjs-charts'
+
+const arrowLeft = require('../images/arrow_left_black.png')
+const arrowRight = require('../images/arrow_right_black.png')
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                'August', 'September', 'October', 'November', 'December']
 
 export default class Dashboard extends Component{
      constructor(props) {
@@ -13,7 +21,8 @@ export default class Dashboard extends Component{
         this.state = {
             data: '',
             sortCategory: '',
-            tempTrans: [[{'x': 0, 'y': 0}]]
+            tempTrans: [[{'x': 0, 'y': 0}]],
+            currentDate: new Date(),            
         }
         this.transRef = Firebase.database().ref().orderByChild('userId').equalTo(this.props.userInfo.userId);
     }
@@ -59,55 +68,77 @@ export default class Dashboard extends Component{
                 </View>
             )
     }
-    render(){
-    let optionsLine = {
-      width: 250,
-      height: 250,
-      color: '#2980B9',
-      margin: {
-        top: 10,
-        left: 35,
-        bottom: 30,
-        right: 10
-      },
-      animate: {
-        type: 'delayed',
-        duration: 200
-      },
-      axisX: {
-        showAxis: true,
-        showLines: true,
-        showLabels: true,
-        showTicks: true,
-        zeroAxis: false,
-        orient: 'bottom',
-        tickValues: [],
-        label: {
-          fontFamily: 'Arial',
-          fontSize: 8,
-          fontWeight: true,
-          fill: '#34495E'
-        }
-      },
-      axisY: {
-        showAxis: true,
-        showLines: true,
-        showLabels: true,
-        showTicks: true,
-        zeroAxis: false,
-        orient: 'left',
-        tickValues: [],
-        label: {
-          fontFamily: 'Arial',
-          fontSize: 8,
-          fontWeight: true,
-          fill: '#34495E'
-        }
-      }
+    onNextPress() {
+        console.log('Next is pressed')
     }
+    onPrevPress() {
+        console.log('Prev is pressed')
+    }
+    render(){
+        let optionsLine = {
+            width: 250,
+            height: 250,
+            color: '#2980B9',
+            margin: {
+                top: 10,
+                left: 35,
+                bottom: 30,
+                right: 10
+            },
+            animate: {
+                type: 'delayed',
+                duration: 200
+            },
+            axisX: {
+                showAxis: true,
+                showLines: true,
+                showLabels: true,
+                showTicks: true,
+                zeroAxis: false,
+                orient: 'bottom',
+                tickValues: [],
+                label: {
+                fontFamily: 'Arial',
+                fontSize: 8,
+                fontWeight: true,
+                fill: '#34495E'
+                }
+            },
+            axisY: {
+                showAxis: true,
+                showLines: true,
+                showLabels: true,
+                showTicks: true,
+                zeroAxis: false,
+                orient: 'left',
+                tickValues: [],
+                label: {
+                fontFamily: 'Arial',
+                fontSize: 8,
+                fontWeight: true,
+                fill: '#34495E'
+                }
+            }
+        }
         return(
             <View style={styles.container}>
-                <Text style={styles.text}> {this.props.email} </Text>
+                <View style={styles.nav}>
+                    <TouchableHighlight
+                        onPress={this.onPrevPress.bind(this)}>
+                        <View style={styles.buttonWrapper}>
+                            <Image source={arrowLeft}/>
+                            <Text>Prev</Text>
+                        </View>
+                    </TouchableHighlight>
+                    <Text style={styles.text}> {months[this.state.currentDate.getMonth()]} </Text>
+                    <TouchableHighlight
+                        onPress={this.onNextPress.bind(this)}>
+                        <View style={styles.buttonWrapper}>
+                            <Text>Next</Text>
+                            <Image source={arrowRight}/>
+                        </View>
+                    </TouchableHighlight>
+                </View>
                 {this.setData(optionsLine, this.state.tempTrans)}
             </View>
         )
@@ -118,6 +149,17 @@ const styles = StyleSheet.create({
   container: {
     flex:1
   },
+  nav: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginTop: (Platform.OS === 'ios') ? 30 : 0,   
+  },
+  buttonWrapper: {
+      flexDirection: 'row',
+      margin: 10,
+  },
+  buttonText: {},
   text:{
       paddingVertical: 30,
       textAlign: "center"
