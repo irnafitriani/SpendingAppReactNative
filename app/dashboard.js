ef57cb9c2fa56f4011d8ebc4a3791400a0e4ac43
Mohammad_T <mohammad.trinoferianto@mitrais.com>
2017-04-03 14:35:19 +0700

    add dropdown to display certain graphic in dashboard

--- app/dashboard.js
+++ app/dashboard.js
@@ -3,12 +3,13 @@ import {
     Image,
     View,
     Text,
+    Picker,
     Platform,
     StyleSheet,
     TouchableHighlight,
 } from 'react-native'
 import Firebase from 'firebase'
-import { Bar, StockLine, SmoothLine } from 'react-native-pathjs-charts'
+import { Bar, StockLine, SmoothLine, Pie } from 'react-native-pathjs-charts'
 const background = require("../images/background.jpg");
 
 const arrowLeft = require('../images/arrow_left_white.png')
@@ -22,8 +23,10 @@ export default class Dashboard extends Component{
         super(props)
         this.state = {
             data: '',
-            sortCategory: '',
-            tempTrans: [[{'x': 0, 'y': 0}]],
+            sortCategory: 'All',
+            // tempTrans: [[{'x': 0, 'y': 0}]],
+            tempTransLine: [],
+            tempTransPie: [],
             currentMonth: new Date().getMonth(), 
             nextDisable: true,
             prevDisable: false,
@@ -39,10 +42,13 @@ export default class Dashboard extends Component{
         transRef.on('value', (transactions) => {
             // initialize the graphic start point
             var newTransactions = [{'x': 0, 'y': 0}];
+            var counter = 0
 
             // add transactions to the graphic
             transactions.forEach((transaction) => {
                 if(transaction.val().userId === this.props.userInfo.userId) {
+                    counter = counter + 1
+
                     // check if its on the same day with the last pushed transaction
                     var date = transaction.val().date.substr(8)
                     var amount = parseInt(transaction.val().amount)
@@ -65,70 +71,82 @@ export default class Dashboard extends Component{
             // add the graphic end point
             newTransactions.push({'x': this.state.currentMonth === 1 ? 30 : 35, 'y': 0})
 
-            // update tempTrans state
-            this.setState({
-                tempTrans: [newTransactions],
-            })
+            // update tempTransLine state
+            if(counter > 0) {
+                this.setState({
+                    tempTransLine: [newTransactions],
+                })
+                console.log('set temp trans line')
+            } else {
+                this.setState({
+                    tempTransLine: [],
+                })
+            }
         })
     }
-    setData(optionsLine, dataLine){
-            return(
-                <View>
-                 <StockLine data={this.state.tempTrans} options={optionsLine} xKey='x' yKey='y'  />
-                </View>
-            )
-    }
-    onNextPress() {
-        if(!this.state.nextDisable) {
-            var month = this.state.currentMonth + 1;
-            if(month > 11) {
-                month = month - 12
+    listenForTransRefPie(transRef) {
+        transRef.on('value', (transactions) => {
+            // initialize the graphic start point
+            var categories = ['cat 1', 'cat 2', 'cat 3', 'none']
+            var amounts = [0, 0, 0, 0]
+            var counter = 0
+
+            // add transactions to the graphic
+            transactions.forEach((transaction) => {
+                if(transaction.val().userId === this.props.userInfo.userId) {
+                    counter = counter + 1
+                    var cat = transaction.val().category === undefined ? 'none' : transaction.val().category
+                    var amount = parseInt(transaction.val().amount)
+
+                    if(cat === categories[0]) {
+                        amounts[0] = amounts[0] + amount
+                    } else if(cat === categories[1]) {
+                        amounts[1] = amounts[1] + amount
+                    } else if(cat === categories[2]) {
+                        amounts[2] = amounts[2] + amount
+                    } else {
+                        amounts[3] = amounts[3] + amount
+                    }
+                }
+            })
+
+            let data = [{
+                'name': categories[0],
+                'total': amounts[0]
+            }, {
+                'name': categories[1],
+                'total': amounts[1]
+            }, {
+                'name': categories[2],
+                'total': amounts[2]
+            }, {
+                'name': categories[3],
+                'total': amounts[3]
+            }]
+
+            // update tempTransPie state
+            if(counter > 0) {
+                this.setState({
+                    tempTransPie: data,
+                })
+                console.log('set temp trans pie')
+            } else {
+                this.setState({
+                    tempTransPie: [],
+                })
             }
-            this.setState({currentMonth: month})
-            this.getSelectedMonthData(month)
-            this.disablePrevButtonNav(month)
-            this.disableNextButtonNav(month)
+        })
+    }
+    setData() {
+        console.log('set data')
+        if(this.state.sortCategory === 'All') {
+            return this.setDataLine()
+        } else {
+            return this.setDataPie()
         }
     }
-    onPrevPress() {
-        if(!this.state.prevDisable){
-            var month = this.state.currentMonth - 1;
-            if(month < 0) {
-                month = month + 12
-            }
-            this.setState({currentMonth: month})
-            this.getSelectedMonthData(month)
-            this.disableNextButtonNav(month)
-            this.disablePrevButtonNav(month)
-        }
-    }
-    disablePrevButtonNav(month){
-        var result = month === 0 ? true : false
-        this.setState({prevDisable: result})
-    }
-    disableNextButtonNav(month) {
-        var result = month === new Date().getMonth() ? true : false
-        this.setState({nextDisable: result})
-    }
-    getSelectedMonthData(month) {
-        // get min date
-        var minDate = new Date()        
-        minDate.setDate(1)
-        minDate.setMonth(month)
-        console.log('Min Date ' + minDate.toISOString())
-
-        // get max date
-        var maxDate = new Date()
-        var max = (month !== 1) ? maxDates[month] : (maxDate.getFullYear() % 4 === 0) ? 29 : 28
-        maxDate.setDate(max)
-        maxDate.setMonth(month)
-        console.log('Max Date ' + maxDate.toISOString())
-
-        // get the data filtered by current month
-        this.listenForTaskRef(this.transRef.ref.orderByChild('date').startAt(minDate.toISOString()).endAt(maxDate.toISOString()))
-    }
-    render(){
-        let optionsLine = {
+    setDataLine(){
+        let options = {
             width: 250,
             height: 250,
             color: '#ffffff',
@@ -173,6 +191,142 @@ export default class Dashboard extends Component{
                 }
             }
         }
+        console.log('line - category ', this.state.sortCategory)
+        console.log(this.state.tempTransLine)
+        if(this.state.tempTransLine.length === 0) {
+            return(
+                <View>
+                    <Text>No data to display</Text>
+                </View>
+            )
+        } else {
+            return(
+                <View>
+                    <StockLine data={this.state.tempTransLine} options={options} xKey='x' yKey='y'  />
+                </View>
+            )
+        }
+    }
+    setDataPie() {
+        let options = {
+            margin: {
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: 20
+            },
+            width: 320,
+            height: 320,
+            color: '#2980B9',
+            r: 50,
+            R: 150,
+            legendPosition: 'topLeft',
+            animate: {
+                type: 'oneByOne',
+                duration: 200,
+                fillTransition: 3
+            },
+            label: {
+                fontFamily: 'Arial',
+                fontSize: 8,
+                fontWeight: true,
+                color: '#ECF0F1'
+            }
+        }
+        console.log('pie - category ', this.state.sortCategory)
+        console.log(this.state.tempTransPie)
+        if(this.state.tempTransPie.length === 0) {
+            return(
+                <View>
+                    <Text>No data to display</Text>
+                </View>
+            )
+        } else {
+            return(
+                <View>
+                    <Pie 
+                        data={this.state.tempTransPie} 
+                        options={options} 
+                        accessorKey="total"
+                        margin={{top: 20, left: 20, right: 20, bottom: 20}}
+                        color="#2980B9"
+                        pallete={[
+                            {'r':25,'g':99,'b':201},
+                            {'r':24,'g':175,'b':35},
+                            {'r':190,'g':31,'b':69},
+                            {'r':100,'g':36,'b':199},
+                            {'r':214,'g':207,'b':32},
+                            {'r':198,'g':84,'b':45}
+                        ]}
+                        r={50}
+                        R={150}
+                        label={{
+                            fontFamily: 'Arial',
+                            fontSize: 8,
+                            fontWeight: true,
+                            color: '#ECF0F1'
+                        }}
+                    />
+                </View>
+            )
+        }
+    }
+    onNextPress() {
+        if(!this.state.nextDisable) {
+            var month = this.state.currentMonth + 1;
+            if(month > 11) {
+                month = month - 12
+            }
+            this.setState({currentMonth: month})
+            this.getSelectedMonthData(month, this.state.category)
+            this.disablePrevButtonNav(month)
+            this.disableNextButtonNav(month)
+        }
+    }
+    onPrevPress() {
+        if(!this.state.prevDisable){
+            var month = this.state.currentMonth - 1;
+            if(month < 0) {
+                month = month + 12
+            }
+            this.setState({currentMonth: month})
+            this.getSelectedMonthData(month, this.state.category)
+            this.disableNextButtonNav(month)
+            this.disablePrevButtonNav(month)
+        }
+    }
+    disablePrevButtonNav(month){
+        var result = month === 0 ? true : false
+        this.setState({prevDisable: result})
+    }
+    disableNextButtonNav(month) {
+        var result = month === new Date().getMonth() ? true : false
+        this.setState({nextDisable: result})
+    }
+    getSelectedMonthData(month) {
+        // get min date
+        var minDate = new Date()
+        minDate.setMonth(month)
+
+        // get max date
+        var maxDate = new Date()
+        var max = (month !== 1) ? maxDates[month] : (maxDate.getFullYear() % 4 === 0) ? 29 : 28
+        maxDate.setMonth(month)
+        maxDate.setDate(max)
+
+        // get the data filtered by current month
+        if(this.state.sortCategory === 'All') {
+            this.listenForTaskRef(this.transRef.ref.orderByChild('date').startAt(minDate.toISOString()).endAt(maxDate.toISOString()))
+        } else {
+            this.listenForTransRefPie(this.transRef.ref.orderByChild('date').startAt(minDate.toISOString()).endAt(maxDate.toISOString()))
+        }
+    }
+    pickerChange(category) {
+        // set sort category
+        this.getSelectedMonthData(this.state.currentMonth)
+        this.setState({sortCategory: category})
+    }
+    render(){
         return(
             <Image 
                 style={[styles.background, styles.container]}
@@ -198,7 +352,15 @@ export default class Dashboard extends Component{
                             </View>
                         </TouchableHighlight>
                     </View>
-                    {this.setData(optionsLine, this.state.tempTrans)}
+                    <View >
+                        <Picker
+                            selectedValue={this.state.sortCategory}
+                            onValueChange={(val) => this.pickerChange(val)}>
+                            <Picker.Item label='All' value='All'/>
+                            <Picker.Item label='Category' value='Category'/>
+                        </Picker>
+                    </View>
+                    {this.setData()}
                 </View>
             </Image>
         )
