dd5692bbc35d708a29b95bc00a5c4d44bab34f11
MTMAC21 <mtmac21@mtmac21s-mini.mitrais.com>
2017-04-21 13:36:14 +0700

    create api for currency exchange

---
+++ app/Helpers/api.js
@@ -0,0 +1,28 @@
+class Api {
+  static headers() {
+    return {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'dataType': 'json',
+    }
+  }
+
+  static get(route) {
+    return this.xhr(route, null, 'GET');
+  }
+
+  static xhr(route, params, verb) {
+    const host = 'http://api.fixer.io/latest'
+    const url = `${host}${route}`
+    let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null );
+    options.headers = Api.headers()
+    return fetch(url, options).then( resp => {
+      let json = resp.json();
+      if (resp.ok) {
+        return json
+      }
+      return json.then(err => {throw err});
+    }).then( json => json.results );
+  }
+}
+export default Api
