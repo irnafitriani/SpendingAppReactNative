e358a5a25315bc1c90cb88b8970789d2b44c491c
Mohammad_T <mohammad.trinoferianto@mitrais.com>
2017-04-26 08:54:46 +0700

    add forgot password, registration unit tests for android

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,12 +1,19 @@
 import 'react-native';
-import React from 'react';
-import Index from '../index.android.js';
+import React from 'react'
+import Registration from './registration.test.js'
+import ForgotPassword from './forgotPassword.test.js'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
-});
+// it('renders correctly', () => {
+//     <Registration/>
+//     // <ForgotPassword/>
+// });
+
+describe('Addition', () => {
+    it('knows that 2 add 2 make 4', () => {
+        expect(2 + 2).toBe(6)
+    })
+})
+
